b32f2aa66ffc2d0ea112be9d7c4c918a09d321ce
Abdullah Esmael <abdullahesmael98@gmail.com>
2022-08-18 14:40:04 +0200

    added icon

--- components/TodoItem.js
+++ components/TodoItem.js
@@ -1,9 +1,13 @@
-import { StyleSheet, Text, TouchableOpacity,  } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View,  } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
 
 function TodoItem({ item, pressHandler }) {
     return (
         <TouchableOpacity onPress={() => pressHandler(item.key)}>
-            <Text style={styles.item}>{item.text}</Text>
+            <View style={styles.item}>
+                <MaterialIcons name='delete' size={18} color='#333' />
+                <Text style={styles.itemText}>{item.text}</Text>
+            </View>
         </TouchableOpacity>
     );
 }
@@ -11,12 +15,16 @@ function TodoItem({ item, pressHandler }) {
 
 const styles = StyleSheet.create({
     item: {
+        flexDirection: 'row',
         padding: 16,
         marginTop:16,
         borderColor: '#bbb',
         borderWidth: 1,
-        borderStyle: 'dashed',
+        borderStyle: 'solid',
         borderRadius: 10,
+    },
+    itemText: {
+        marginLeft: 10,
     }
 })
 
